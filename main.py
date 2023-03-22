6d31e85127eb43d3007a50568b13621c7c13d3ad
rjthemxguy <rjrseo@gmail.com>
2023-03-22 00:51:48 -0700

    aggregation

--- main.py
+++ main.py
@@ -4,5 +4,27 @@ from pymongo import MongoClient
 
 cluster = MongoClient("mongodb+srv://rj:Hapkido123@cluster0.iiuhn.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["SBC2023"]
-voters = db["Voters"]
+voters = db["voters"]
 
+print ("--- Getting Precincts ---")
+precinctList = (voters.distinct("precinct"))
+
+pipeline = [
+   {
+      "$match": {
+         "precinct": "1458",
+         "party":"AI"
+      }
+   },
+   {
+    "$count": "total"
+  }
+]
+
+
+results = voters.aggregate(pipeline)
+
+for rec in results:
+    print(rec["total"])
+
+    
